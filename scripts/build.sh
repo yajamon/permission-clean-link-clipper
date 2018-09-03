8bc24b6b3c10717f352662dcc4514e6df4f294c9
yajamon <yajamon.tatsuki@gmail.com>
2018-09-03 19:50:25 +0900

    Add build script

---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# initialize
+echo "## initialize" >&2
+test -d dist || mkdir dist
+
+# cleenup
+echo "## cleanup" >&2
+rm -rfv dist/*
+
+# copy resource
+echo "## copy resource" >&2
+cp -rfv src/* dist/
+
+# compile
