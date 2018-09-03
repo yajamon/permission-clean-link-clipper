17e78e5450c43b0168a14307c38fdfdd58bab00f
yajamon <yajamon.tatsuki@gmail.com>
2018-09-03 21:59:44 +0900

    popup.htmlにDOM取得とblowserActionのクリック検知を入れてみる
    BlowserAction.onClickedが動いていない

--- src/ts/popup.ts
+++ src/ts/popup.ts
@@ -1,4 +1,18 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    let clipbase = document.getElementById("clipbase") as HTMLTextAreaElement;
-    clipbase.innerText = "hello";
+let onClipBaseLoaded = new Promise((resolve)=>{
+    document.addEventListener("DOMContentLoaded", (event) => {
+        let clipbase = document.getElementById("clipbase") as HTMLTextAreaElement;
+        clipbase.innerText = "hello";
+        resolve(clipbase);
+    });
+});
+let onBlowserActionClicked = new Promise((resolve)=>{
+    chrome.browserAction.onClicked.addListener((tab)=> {
+        resolve(tab);
+    });
+});
+
+Promise.all([onClipBaseLoaded, onBlowserActionClicked]).then((values)=>{
+    let clipbase = values[0] as HTMLTextAreaElement;
+    let tab = values[1] as chrome.tabs.Tab;
+    clipbase.innerText = tab.title + "\n" + tab.url;
 });
