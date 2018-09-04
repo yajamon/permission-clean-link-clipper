cb696105b1f597f95ecd7c9c4afb287b910aa908
yajamon <yajamon.tatsuki@gmail.com>
2018-09-04 20:29:26 +0900

    popup.htmlで直近のTabを取得できるようなので、そのように修正

--- src/ts/popup.ts
+++ src/ts/popup.ts
@@ -1,24 +1,20 @@
-import { LatestLocationMessage, LocationInfo } from "./locationinfo";
 
-let onClipBaseLoaded = new Promise((resolve)=>{
-    document.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener("DOMContentLoaded", (event) => {
+    captureTitleAndUrl();
+});
+
+function captureTitleAndUrl() {
+    new Promise((resolve)=>{
+        chrome.tabs.query({
+            active: true,
+        }, (tabs)=> {
+            resolve(tabs.shift());
+        });
+    }).then((tab)=>{
+        let title = (tab as chrome.tabs.Tab).title;
+        let url = (tab as chrome.tabs.Tab).url;
         let clipbase = document.getElementById("clipbase") as HTMLTextAreaElement;
-        clipbase.innerText = "hello";
-        resolve(clipbase);
+        clipbase.innerText = title + "\n" + url;
     });
-});
-let recieveLocationInfo = new Promise((resolve)=>{
-    let message: LatestLocationMessage = {
-        key: "latestLocation",
-        value: undefined,
-    }
-    chrome.runtime.sendMessage(message, (response: LocationInfo)=>{
-        resolve(response);
-    });
-});
 
-Promise.all([onClipBaseLoaded, recieveLocationInfo]).then((values)=>{
-    let clipbase = values[0] as HTMLTextAreaElement;
-    let info = values[1] as LocationInfo;
-    clipbase.innerText = info.title + "\n" + info.url;
-});
+}
