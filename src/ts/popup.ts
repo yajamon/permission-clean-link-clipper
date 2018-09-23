2a36de47b8bca4ead713971a46aabfa28cf0d25b
yajamon <yajamon.tatsuki@gmail.com>
2018-09-24 03:37:38 +0900

    Markdownボタンが機能するようにした

--- src/ts/popup.ts
+++ src/ts/popup.ts
@@ -3,6 +3,14 @@ window.addEventListener("DOMContentLoaded", (event) => {
     captureTitleAndUrlAsync()
         .then(generateDefaultFormat)
         .then(copyToClipboard);
+
+    for (let element of document.getElementsByClassName("copyButton")) {
+        element.addEventListener("click", () => {
+            captureTitleAndUrlAsync()
+                .then(generateMarkdownFormat)
+                .then(copyToClipboard);
+        });
+    }
 });
 
 function captureTitleAndUrlAsync() {
@@ -25,6 +33,10 @@ function captureTitleAndUrlAsync() {
 function generateDefaultFormat(tab: chrome.tabs.Tab) {
     let clipbase = document.getElementById("clipbase") as HTMLTextAreaElement;
     clipbase.innerText = tab.title + "\n" + tab.url;
+}
+function generateMarkdownFormat(tab: chrome.tabs.Tab) {
+    let clipbase = document.getElementById("clipbase") as HTMLTextAreaElement;
+    clipbase.innerText = `[${tab.title}](${tab.url})`;
 }
 
 function copyToClipboard() {
