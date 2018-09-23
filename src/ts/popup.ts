adeb4e0ebdd9a772c224ab35937fbac40e04a60d
yajamon <yajamon.tatsuki@gmail.com>
2018-09-24 03:12:16 +0900

    Promiseの型情報を補完した

--- src/ts/popup.ts
+++ src/ts/popup.ts
@@ -6,18 +6,21 @@ window.addEventListener("DOMContentLoaded", (event) => {
 });
 
 function captureTitleAndUrlAsync() {
-    return new Promise((resolve) => {
+    return new Promise((resolve: (tab: chrome.tabs.Tab) => void, reject) => {
         chrome.tabs.query({
             active: true,
             currentWindow: true,
         }, (tabs) => {
-            resolve(tabs.shift());
+            let tab = tabs.shift()
+            if (!tab) {
+                reject("tab not found!");
+                return;
+            }
+            resolve(tab);
         });
     }).then((tab) => {
-        let title = (tab as chrome.tabs.Tab).title;
-        let url = (tab as chrome.tabs.Tab).url;
         let clipbase = document.getElementById("clipbase") as HTMLTextAreaElement;
-        clipbase.innerText = title + "\n" + url;
+        clipbase.innerText = tab.title + "\n" + tab.url;
         return true;
     });
 
